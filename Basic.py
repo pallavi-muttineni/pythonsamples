d6de2b5f8793d549da08a9580965cf6786a556c0
pallavi-muttineni <pallavimuttineni@gmail.com>
2025-06-30 14:16:44 +0530

    first commit

---
+++ Basic.py
@@ -0,0 +1,34 @@
+# print("hello")
+# a=10
+# b,c=20,30
+# d=e=f=40
+# print(a)
+# print(b)
+# a=int(input("Enter a value"))
+# b=int(input("Enter b value"))
+# print(a+b)
+# d,e,f=map(int,input("enter d,e,f values").split())
+# age=18
+# if age>18:
+#     print("Eligible")
+# elif age==18:
+#     print("Welcome new voter")
+# else:
+#     print("Not Eligible")
+# n=int(input("enter pretty number"))
+# if n%10==7 or n/10==7:
+#     print("Pretty number")
+# else:
+#     print("No")
+# for i in range(10,99):
+#     if i%10==7 or i//10==7:
+#         print(i,end="")
+# list1=[10,20,False]
+# print(sum(list1))
+n=10
+list1=[]
+for i in range(n):
+    if i%2==0:
+        list1.append(i)
+print(list1)
+        
